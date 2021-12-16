5fe3fa93d385b977b3e068ae431f634ca518cc04
Sam Forbyn <samforbyn@gmail.com>
2021-12-16 14:22:33 -0700

    added rollbar / set up

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const path = require('path')
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '8e48320c64264eecb2cbd07758a512ce',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
 const app = express()
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file serverd successfully')
 })
+
+app.use(rollbar.errorHandler())
 
 const port = process.env.PORT || 4545
 
