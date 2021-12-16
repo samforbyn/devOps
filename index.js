93b39fb271fac2dc0b7cbb35cf10cf1f47e9f729
Sam Forbyn <samforbyn@gmail.com>
2021-12-16 13:54:21 -0700

    fix port

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.json)
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-const port = process.env.PORT || 4747
+const port = process.env.PORT || 4545
 
 app.listen(port, () => console.log(`The horses name was port ${port}`))
